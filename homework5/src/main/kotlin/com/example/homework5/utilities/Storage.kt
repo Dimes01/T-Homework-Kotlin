8caff91c3290033ac74bf04933b711fd7b7c63a3
Dimes01 <tyapin2002@mail.ru>
2024-09-25 20:01:29 +0400

    feat: написан CategoryController
    1. написан контроллер
    2. исправлен Storage

--- homework5/src/main/kotlin/com/example/homework5/utilities/Storage.kt
+++ homework5/src/main/kotlin/com/example/homework5/utilities/Storage.kt
@@ -4,43 +4,46 @@ import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.util.concurrent.ConcurrentHashMap
 
-/**
- * Возник вопрос по поводу рациональнсти данной реализации, а именно:
- *
- * **Стоит ли использовать `hashcode()` для определения `id` для хранилища?**
- *
- * До этого рассматривал варианты:
- * 1. Использовать, например, `AtomicLong` для генерации `id`, но тогда вставал вопрос о том,
- * как узнать `id` объекта, который передали в параметре методам `save()` и `update()`?
- * 2. Создать интерфейс, например, `ModelWithId` и наследовать `Category` и `Location` от него,
- * но как будто с основными сущностями этого делать не стоит.
- * 3. Классы-прослойки, которые наследуются от сущностей и реализуют интерфейс `ModelWithId`,
- * но это показалось муторно...
- */
 @Component
 class Storage<T> {
-    private val storage = ConcurrentHashMap<Int, T>()
+    private val storage = ConcurrentHashMap<Long, T>()
     private val logger = LoggerFactory.getLogger(Storage::class.java)
 
-    private fun getIdByEntity(entity: T) = entity.hashCode()
-
     fun getAll(): List<T> {
-        logger.info("Method 'getAll': started and finished")
+        logger.info("Method 'getAll': started")
+        logger.info("Method 'getAll': finished")
         return storage.values.toList()
     }
 
-    fun save(entity: T): T {
+    fun getById(id: Long): T? {
+        logger.info("Method 'getById': started")
+        val elem = storage[id]
+        if (elem == null)
+            logger.warn("Method 'getById': required element is not found")
+        logger.info("Method 'getById': finished")
+        return elem
+    }
+
+//    // Из-за того, что у Location такого же id, как у Category нет, то решил делегировать правило выбора наружу
+//    fun getByRule(block: (T) -> Boolean): T? {
+//        logger.info("Method 'getByRule': started")
+//        val elem = storage.values.find { block(it) }
+//        if (elem == null)
+//            logger.warn("Method 'getByRule': required element is not found")
+//        logger.info("Method 'getByRule': finished")
+//        return elem
+//    }
+
+    fun save(id: Long, entity: T): T {
         logger.info("Method 'save': started")
-        val id = getIdByEntity(entity)
         storage[id] = entity
         logger.debug("Method 'save': entity with id($id) saved")
         logger.info("Method 'save': finished")
         return entity
     }
 
-    fun update(entity: T): T? {
+    fun update(id: Long, entity: T): T? {
         logger.info("Method 'update': started")
-        val id = getIdByEntity(entity)
         val result = if (storage.containsKey(id)) {
             storage[id] = entity
             logger.debug("Method 'update': result is entity with id($id)")
@@ -53,9 +56,8 @@ class Storage<T> {
         return result
     }
 
-    fun delete(entity: T): Boolean {
+    fun delete(id: Long, entity: T): Boolean {
         logger.info("Method 'delete': started")
-        val id = getIdByEntity(entity)
         val result = storage.remove(id) != null
         if (result)
             logger.debug("Method 'delete': deleted entity with id($id)")
