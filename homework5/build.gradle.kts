0783fc5a7a6b84f91ed39e7a5de86cd1627202f3
Dimes01 <tyapin2002@mail.ru>
2024-09-29 18:00:40 +0400

    fix: исправлен Storage
    1. Исправлен Storage.kt
    2. Исправлены контроллеры под новый Storage

--- homework5/build.gradle.kts
+++ homework5/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "2.0.0"
 	id("org.springframework.boot") version "3.3.4"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("jacoco")
 }
 
 group = "com.example"
@@ -38,6 +39,20 @@ kotlin {
 	}
 }
 
-tasks.withType<Test> {
+tasks.test {
 	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
+
+}
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required.set(false)
+		csv.required.set(false)
+	}
+	classDirectories.setFrom(files(classDirectories.files.map {
+		fileTree(it) {
+			exclude("**/models/**")
+		}
+	}))
 }
